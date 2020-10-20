6a74b69b200fd55a16beea471430e7dcce4ab8c0
Daniel Villa <2057@holbertonschool.com>
2020-10-20 11:16:59 -0500

    selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - Sort with selection algorithm
+ *
+ * @array: The array
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, tmp;
+	int tmps;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0 ; i < size - 1 ; i++)
+	{
+		tmp = i;
+		for (j = i + 1 ; j < size; j++)
+		{
+			if (array[j] < array[tmp])
+				tmp = j;
+		}
+		tmps = array[i];
+		array[i] = array[tmp];
+		array[tmp] = tmps;
+		print_array(array, size);
+	}
+}
