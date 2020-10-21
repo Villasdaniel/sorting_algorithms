ec4d2e5a330205a1cf045590742fbad3968df812
Daniel Villa <2057@holbertonschool.com>
2020-10-21 14:04:26 -0500

    actualized header

--- sort.h
+++ sort.h
@@ -23,5 +23,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void quick_sort_complement(int *array, size_t size, int p_low, int p_high);
+int partition(int *array, size_t size, int p_low, int p_high);
 #endif
